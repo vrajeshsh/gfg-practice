e482074d3941cb86b22958cbe9a120608b793f48
Vrajesh <vrajeshshah13@gmail.com>
2022-06-03 08:29:53 -0400

    Added solution - LeetHub

--- Palindromic Array - GFG/palindromic-array.py
+++ Palindromic Array - GFG/palindromic-array.py
@@ -1,31 +1,12 @@
 # Your task is to complete this function
 # Function should return True/False or 1/0
+def PalinArray(arr ,n):
+    # Code here
+    for i in (arr):
+        if str(i)!=str(i)[::-1]:
+            return False
+    return True
 
-# Python3 implementation to check
-# if an array is PalinArray or not
-
-# Function to check if palindrome or not
-def isPalindrome(N):
-	str1 = "" + str(N)
-	len1 = len(str1)
-	for i in range(int(len1 / 2)):
-		if (str1[i] != str1[len1 - 1 - i]):
-			return False
-	return True
-
-# Function to check
-# if an array is PalinArray or not
-def PalinArray(arr, n):
-	
-	# Traversing each element of the array
-	# and check if it is palindrome or not
-	for i in range(n):
-		ans = isPalindrome(arr[i])
-		if (ans == False):
-			return False
-	return True
-
-# This code is contributed by PrinciRaj1992
 
 #{ 
 #  Driver Code Starts
