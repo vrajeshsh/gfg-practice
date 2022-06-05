8b254056fec111278aab10c3bb9c943c3dd15887
Vrajesh <vrajeshshah13@gmail.com>
2022-06-05 09:23:17 -0400

    Added solution - LeetHub

---
+++ Lower case to upper case - GFG/lower-case-to-upper-case.py
@@ -0,0 +1,17 @@
+#User function Template for python3
+class Solution:
+    def to_upper(self, str):
+        # code 
+        return str.upper()
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        str = input().strip()
+        ob = Solution()
+        print(ob.to_upper(str))
+# } Driver Code Ends
